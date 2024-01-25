9336afc7c31f1bd4c8168f9882e111c1318843c3
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-25 13:14:12 +0100

    styled layout

--- src/pages/Layout/Layout.styled.jsx
+++ src/pages/Layout/Layout.styled.jsx
@@ -1,16 +1,7 @@
-import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
-
-*,*::before,*::after{
-    margin:0;
-    padding: 0;
-    border:none;
-    box-sizing:border-box;
-    
-   
-}
-
-
-
+export const StyledLayout = styled.div`
+  background-color: indianred;
+  width: 100vw;
+  height: 100vh;
 `;
