4b0355dbac427f1e0387b8240648c278c631dea9
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-25 08:27:09 +0100

    react-dom/client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.scss";
 import "bootstrap/dist/js/bootstrap.bundle";
