4e5a3059cb112cc2f9a6284623353c21b4da739c
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-25 11:02:30 +0100

    locomotive scroll

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const HomePage = styled.div`
+    background-color: red;
+
+    height: 200vh;
+  `;
+
+  return (
+    <HomePage className="container-fluid">
+      <h1>Home</h1>
+    </HomePage>
+  );
 };
 
 export default Home;
