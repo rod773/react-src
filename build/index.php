ad455faf84da6f4034924a34b99d965e88d01c8d
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-26 16:28:07 +0100

    images margin

--- build/index.php
+++ build/index.php
@@ -39,7 +39,7 @@
         Learn how to configure a non-root public URL by running `npm run wpbuild`.
     -->
     <title>React WordPress Theme</title>
-<link href="/wp-content/themes/tienda/static/css/main.chunk.css?9a1bc6d199969db5c694" rel="stylesheet"></head>
+<link href="/wp-content/themes/tienda/static/css/main.chunk.css?dd98631cc519931b2095" rel="stylesheet"></head>
     <body>
     <noscript>
         You need to enable JavaScript to run this app.
@@ -55,5 +55,5 @@
         To begin the development, run `npm run wpstart` or `yarn wpstart`.
         To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
     -->
-    <script src="/wp-content/themes/tienda/static/js/bundle.js?9a1bc6d199969db5c694"></script><script src="/wp-content/themes/tienda/static/js/0.chunk.js?9a1bc6d199969db5c694"></script><script src="/wp-content/themes/tienda/static/js/main.chunk.js?9a1bc6d199969db5c694"></script></body>
+    <script src="/wp-content/themes/tienda/static/js/bundle.js?dd98631cc519931b2095"></script><script src="/wp-content/themes/tienda/static/js/0.chunk.js?dd98631cc519931b2095"></script><script src="/wp-content/themes/tienda/static/js/main.chunk.js?dd98631cc519931b2095"></script></body>
 </html>
