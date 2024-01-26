aea96515e719c869ddb527564a14dd4389596fdf
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-26 12:31:51 +0100

    more images

--- src/Images.js
+++ src/Images.js
@@ -1,15 +1,30 @@
-import image1 from "./images/1-Enero-2016-Elefante.jpeg";
-import image2 from "./images/2-Sabana-65x30-2015-2-150x150.jpeg";
-import image3 from "./images/3-caballo-50x40-2015-10.jpeg";
-import image4 from "./images/4-Las-cabezas-2015-11.jpeg";
-import image5 from "./images/5-Torero-30x25-2016-3.jpeg";
-import image6 from "./images/6-Elvis-30x25-2016-5.jpeg";
-import image7 from "./images/7-Marilyn-30x25-2016-7.jpeg";
-import image8 from "./images/8-Campo-de-girasoles-30x20-2016-9-150x150.jpeg";
-import image9 from "./images/9-bodegon-70x50-2016-10-300x300.jpeg";
-import image10 from "./images/10-Beso-klim-Vertical-50x40-2016-9-150x150.jpeg";
-import image11 from "./images/11-Beso-klimt-50x40-2017-12-150x150.jpeg";
-import image12 from "./images/12-Bailaria-Cristal-30x40-2017-1-150x150.jpeg";
+import image1 from "./images/1.jpeg";
+import image2 from "./images/2.jpeg";
+import image3 from "./images/3.jpeg";
+import image4 from "./images/4.jpeg";
+import image5 from "./images/5.jpeg";
+import image6 from "./images/6.jpeg";
+import image7 from "./images/7.jpeg";
+import image8 from "./images/8.jpeg";
+import image9 from "./images/9.jpeg";
+import image10 from "./images/10.jpeg";
+import image11 from "./images/11.jpeg";
+import image12 from "./images/12.jpeg";
+import image13 from "./images/13.jpeg";
+import image14 from "./images/14.jpeg";
+import image15 from "./images/15.jpeg";
+import image16 from "./images/16.jpeg";
+import image17 from "./images/17.jpeg";
+import image18 from "./images/18.jpeg";
+import image19 from "./images/19.jpeg";
+import image20 from "./images/20.jpeg";
+import image21 from "./images/21.jpeg";
+import image22 from "./images/22.jpeg";
+import image23 from "./images/23.jpeg";
+import image24 from "./images/24.jpeg";
+import image25 from "./images/25.jpeg";
+import image26 from "./images/26.jpeg";
+import image27 from "./images/27.jpeg";
 
 const images = [
   image1,
@@ -24,6 +39,21 @@ const images = [
   image10,
   image11,
   image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
 ];
 
 export default images;
